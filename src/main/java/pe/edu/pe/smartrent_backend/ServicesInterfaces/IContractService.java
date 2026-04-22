0105844ff4988a24ace38129b593b9e80e8746d9
777fsc777-cpu <777fsc777@gmail.com>
2026-04-22 17:29:55 -0500

    Parte del los ServicesInterfaces del Contract

---
+++ src/main/java/pe/edu/pe/smartrent_backend/ServicesInterfaces/IContractService.java
@@ -0,0 +1,15 @@
+package pe.edu.pe.smartrent_backend.ServicesInterfaces;
+
+import pe.edu.pe.smartrent_backend.Entities.Contract;
+
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IContractService {
+    List<Contract> list();
+    Contract insert(Contract c);
+    Optional<Contract> listId(int id);
+    void update(Contract c);
+    void delete(int id);
+}
