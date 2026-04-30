a98c6a1776432381816b73bc623289552e326f5d
LuisG <luisguillermoa8@gmail.com>
2026-04-30 03:05:54 -0500

    Cambio de limite de contraseña

--- src/main/java/pe/edu/pe/smartrent_backend/Entities/User.java
+++ src/main/java/pe/edu/pe/smartrent_backend/Entities/User.java
@@ -27,7 +27,7 @@ public class User implements Serializable {
     @Column(name = "username", length = 20, nullable = false)
     private String username;
 
-    @Column(name = "password", length = 50, nullable = false)
+    @Column(name = "password", length = 1000, nullable = false)
     private String password;
 
     @Column(name = "statusVerification", nullable = false)
