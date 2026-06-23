3a8f308822aecd7561b9ec1ed30669ed13b426b6
LuisG <luisguillermoa8@gmail.com>
2026-06-23 14:10:43 -0500

    Update RoleController.java

--- src/main/java/pe/edu/pe/smartrent_backend/Controllers/RoleController.java
+++ src/main/java/pe/edu/pe/smartrent_backend/Controllers/RoleController.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/RolesController")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class RoleController {
 
     @Autowired
